bbce7ac503a30dbb1e5fbc9845d573dc0acfd681
MFGookey <MFGookey@users.noreply.github.com>
2023-12-04 20:31:15 -0500

    Day 4 part 1

---
+++ src/2023/Day4/LottaLotto/LottaLotto.Core/Card.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LottaLotto.Core
+{
+  public class Card
+  {
+    public int CardId
+    {
+      get; private set;
+    }
+
+    public IEnumerable<int> WinningNumbers
+    {
+      get; private set;
+    }
+
+    public IEnumerable<int> DrawnNumbers
+    {
+      get;
+      private set;
+    }
+
+    public Card(string toParse)
+    {
+      var temp = toParse.Split(':');
+      this.CardId = int.Parse(temp[0].Replace("Card ", string.Empty));
+      temp = temp[1].Split('|');
+      this.WinningNumbers = temp[0].Trim().Replace("  ", " ").Split(" ").Select(n => int.Parse(n.Trim())).ToList();
+      this.DrawnNumbers = temp[1].Trim().Replace("  ", " ").Split(" ").Select(n => int.Parse(n.Trim())).ToList();
+    }
+
+    public int ScoreCard()
+    {
+      var matches = this.WinningNumbers.Join(DrawnNumbers, w => w, d => d, (w, d) => w);
+      if (matches.Any())
+      {
+        return (int)Math.Pow(2, matches.Count() - 1);
+      }
+      return 0;
+    }
+  }
+}
---
+++ src/2023/Day4/LottaLotto/LottaLotto.Core/LottoEvaluator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LottaLotto.Core
+{
+  public class LottoEvaluator
+  {
+    private IEnumerable<Card> _cards;
+    public LottoEvaluator(IEnumerable<string> cardsToParse)
+    {
+      _cards = cardsToParse.Select(s => new Card(s)).ToList();
+    }
+
+    public int GetScore()
+    {
+      return _cards.Select(c => c.ScoreCard()).Sum();
+    }
+  }
+}
