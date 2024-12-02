8e619ff42af34eed9e8bd5619b9eadbd6486e9b3
MFGookey <MFGookey@users.noreply.github.com>
2024-12-02 14:02:35 -0500

    Day 2 part 2

--- src/2024/day02/ReactorReporter/ReactorReporter.Cmd/Program.cs
+++ src/2024/day02/ReactorReporter/ReactorReporter.Cmd/Program.cs
@@ -31,6 +31,21 @@ namespace ReactorReporter.Cmd
       Console.WriteLine(reactorReportEvaluator.CalculateSafeReports(1, 3));
       Console.WriteLine(reactorReportEvaluator.CalculateSafeReports(1, 3, 1));
 
+     reports = @"48 46 47 49 51 54 56
+1 1 2 3 4 5
+1 2 3 4 5 5
+5 1 2 3 4 5
+1 4 3 2 1
+1 6 7 8 9
+1 2 3 4 3
+9 8 7 6 7
+7 10 8 10 11
+29 28 27 25 26 25 22 20".Split('\n').AsEnumerable();
+
+      reactorReportEvaluator = new ReactorReportEvaluator(reports);
+      Console.WriteLine(reactorReportEvaluator.CalculateSafeReports(1, 3));
+      Console.WriteLine(reactorReportEvaluator.CalculateSafeReports(1, 3, 1));
+
       var formatter = new RecordFormatter(reader);
       reports = formatter.FormatFile(Path.Join(AppDomain.CurrentDomain.BaseDirectory, filePath), "\n", true);
       reactorReportEvaluator = new ReactorReportEvaluator(reports);
--- src/2024/day02/ReactorReporter/ReactorReporter.Core/ReactorReportEvaluator.cs
+++ src/2024/day02/ReactorReporter/ReactorReporter.Core/ReactorReportEvaluator.cs
@@ -18,44 +18,61 @@ public class ReactorReportEvaluator
   public int CalculateSafeReports(int minValidDifference, int maxValidDifference, int outOfSpecTolerance){
     int safeCount = 0;
     foreach(var report in _reports) {
-      bool? isAscending = null;
-      int previousValue = report.First();
-      bool isSafe = true;
-      int outOfSpec = 0;
-      foreach(var level in report.Skip(1)){
-
-        if(!isAscending.HasValue) {
-          isAscending = level > previousValue;
-        }
-
-        if(isAscending.Value) {
-          if(level - previousValue < minValidDifference || level - previousValue > maxValidDifference) {
-            outOfSpec++;
-            if(outOfSpec > outOfSpecTolerance) {
-              isSafe = false;
-              break;
-            }
-          } else {
-            previousValue = level;
-          }
-        } else {
-          if(previousValue - level < minValidDifference || previousValue - level > maxValidDifference) {
-            outOfSpec++;
-            if(outOfSpec > outOfSpecTolerance) {
-              isSafe = false;
-              break;
-            }
-          } else {
-            previousValue = level;
-          }
-        }
-      }
-
-      if(isSafe) {
+      if(CalculateSafeReport(report, minValidDifference, maxValidDifference, outOfSpecTolerance)) {
         safeCount++;
       }
     }
 
     return safeCount;
+  }
+
+  private bool CalculateSafeReport(IEnumerable<int> report, int minValidDifference, int maxValidDifference, int outOfSpecTolerance) {
+    if(outOfSpecTolerance < 0) {
+      return false;
+    }
+
+    if(outOfSpecTolerance == 0) {
+      return SafeReportEvaluator(report, minValidDifference, maxValidDifference);
+    }
+
+    bool foundSafe = false;
+
+    for(int index = 0; index < report.Count(); index++) {
+      if(CalculateSafeReport(report.Where((v, i) => i != index), minValidDifference, maxValidDifference, outOfSpecTolerance - 1)) {
+        foundSafe = true;
+        break;
+      }
+    }
+    
+    return foundSafe;
+  }
+
+  private bool SafeReportEvaluator(IEnumerable<int> report, int minValidDifference, int maxValidDifference){
+    bool? isAscending = null;
+    int previousValue = report.First();
+    bool isSafe = true;
+
+    foreach(var level in report.Skip(1)){
+
+      if(!isAscending.HasValue) {
+        isAscending = level > previousValue;
+      }
+
+      if(isAscending.Value) {
+        if(level - previousValue < minValidDifference || level - previousValue > maxValidDifference) {
+          isSafe = false;
+          break;
+        }
+      } else {
+        if(previousValue - level < minValidDifference || previousValue - level > maxValidDifference) {
+          isSafe = false;
+          break;
+        }
+      }
+
+      previousValue = level;
+    }
+
+    return isSafe;
   }
 }
